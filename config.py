3394186efae43046a1d1f73b06ad1c1b691df390
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-04-03 19:22:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","21803165"))
 API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","7403621976"))
+OWNER_ID = int(getenv("OWNER_ID","5487734890"))
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-MUST_JOIN = getenv("MUST_JOIN","WORLD_ALPHA")
+MONGO_DB_URI = getenv("mongodb+srv://supereme:supereme@supreme.pvfksri.mongodb.net/?retryWrites=true&w=majority&appName=supreme")
+MUST_JOIN = getenv("NLTRIDE","NLTRIDE")
